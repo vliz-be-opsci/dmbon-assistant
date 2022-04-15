e3df3f85fa2cf04a23cc073b533d650a31fd0aa2
cedricdecruw <30471340+cedricdcc@users.noreply.github.com>
2022-04-15 23:31:38 +0200

    Update specific_file.js

--- frontend/src/pages/specific_file.js
+++ frontend/src/pages/specific_file.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import {BASE_URL_SERVER} from '../App.js';
 import {useParams} from 'react-router-dom';
 import FileAnnotationView from './../components/file_annotation_view';
+import {Alert} from 'react-bootstrap';
 
 function FileSpecificPage() {
 
@@ -32,22 +33,51 @@ function FileSpecificPage() {
       if (sr.length === 0) {
         return (
           <div>
-            <h1>Loading...</h1>
+            <h1>Checking if requirements are met...</h1>
           </div>
         )
       }else{
         console.log(sr[0]["http://www.w3.org/ns/shacl#conforms"][0]["@value"]);
+        try{var ammount_violations = sr[0]["http://www.w3.org/ns/shacl#result"].length;}
+        catch(error){console.log(error);var ammount_violations = 0;}
+        // start manipulating the sr to be able to render more advanced alerts
+        var manipulated_array = [];
+        for (var i = 0; i < sr.length; i++) {
+          var current_node_dict = {};
+          console.log(sr[i]);
+          console.log(Object.keys(sr[i]));
+          if(Object.keys(sr[i]).includes("@type") && Object.keys(sr[i]).includes("http://www.w3.org/ns/shacl#focusNode")){
+            //get name of predicate from result path 
+            console.log(sr[i]["http://www.w3.org/ns/shacl#resultPath"]);
+            var array_name_predicate = sr[i]["http://www.w3.org/ns/shacl#resultPath"][0]["@id"].split("/");
+            var predicate_name = array_name_predicate[array_name_predicate.length-1];
+            console.log(predicate_name);
+            //get more info of error from sourceshape
+
+          }
+          manipulated_array.push(current_node_dict);
+        }
         if (sr[0]["http://www.w3.org/ns/shacl#conforms"][0]["@value"] === false) {
           return (
-            <div>
-              <h1>Shacl Requirements not met</h1>
-            </div>
+            <>
+              <Alert variant="danger">
+                <Alert.Heading>Requirements not met</Alert.Heading>  
+                <p>
+                  {ammount_violations} predicates are still required for {FileId} to be compliant to the current project.
+                </p>
+              </Alert>
+            </>
           )
         }else{
           return (
-            <div>
-              <h1>Shacl Requirements met</h1>
-            </div>
+            <>
+              <Alert variant="success">
+                <Alert.Heading>Requirements met</Alert.Heading>  
+                <p>
+                  {FileId} is compliant to the current project.
+                </p>
+              </Alert>
+            </>
           )
         }
       }
@@ -65,8 +95,7 @@ function FileSpecificPage() {
 
     return (
         <div>
-            <h5 className="card text-white bg-dark">Space : {SpaceId}</h5>
-            <h5 className="card text-white bg-info">File : {FileId}</h5>
+            <p></p>
             <Alertlogs shacl_requirements={shaclRequirements}/>
             <div>
             <FileAnnotationView listannotations={spaceList}/>
