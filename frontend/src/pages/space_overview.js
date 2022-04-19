9cbe4969e392133bce120344e2f1ad2f54f5ab88
cedricdecruw <30471340+cedricdcc@users.noreply.github.com>
2022-04-19 18:47:41 +0200

    added working graph for the shacl requirement overview

--- frontend/src/pages/space_overview.js
+++ frontend/src/pages/space_overview.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import {BASE_URL_SERVER} from '../App.js';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
+import ReactLoading from 'react-loading';
 function SpaceOverviewPage() {
 
     //define all constants first
@@ -11,6 +12,7 @@ function SpaceOverviewPage() {
     const {SpaceId} = useParams();
     const [ShaclSummary, SetShaclSummary] = useState({});
     const [ShaclOverview, SetShaclOverview] = useState([{}]);
+    const [Loading, setLoading] = useState(true); 
     const [ShaclErrors, SetShaclErrors] = useState(0);
     const [Graphdata, SetGraphdata] = useState({});
 
@@ -46,30 +48,65 @@ function SpaceOverviewPage() {
                 console.log(shacl_requirements);
                 SetShaclOverview(shacl_requirements);
                 SetShaclSummary(res.data.summary);
-                console.log(res.data.summary);
+                console.log(res.data.summary["red"]);
+                //set graphdata
+                SetGraphdata(
+                    {
+                        labels: ['Violations (%)', 'Good (%)', 'Warnings (%)'],
+                        datasets: [
+                          {
+                            label: '# of Votes',
+                            data: [res.data.summary["red"], res.data.summary["green"], res.data.summary["orange"]],
+                            backgroundColor: [
+                              'rgba(255, 99, 132, 0.2)',
+                              'rgba(75, 192, 192, 0.2)',
+                              'rgba(255, 159, 64, 0.2)',
+                            ],
+                            borderColor: [
+                              'rgba(255, 99, 132, 1)',
+                              'rgba(75, 192, 192, 1)',
+                              'rgba(255, 159, 64, 1)',
+                            ],
+                            borderWidth: 1,
+                          },
+                        ],
+                      }
+                )
                 //get the number of shacl errors that are in shacl_requirements
                 try{var ammount_violations = shacl_requirements[0]["http://www.w3.org/ns/shacl#result"].length;console.log(ammount_violations);SetShaclErrors(ammount_violations);}
                 catch(error){console.log(error);var ammount_violations = 0;}
+                setLoading(false);
             })
-        }   
+        }  
+        
     }
 
     useEffect(() => {
         fetchShaclOverview();
     }, [])
 
-    return (
-        <div>
-            <h1>TODO: dashboard space </h1>
-            <ol>
-                <li>semantic progress overview</li>
-                <li>git overview</li>
-                <li>publish overview</li>
-            </ol>
-            <a href={`/spaces/${SpaceId}/all_files`}>see all files</a>
-            <Doughnut data={dummydata} />
-        </div>
-    )
+    if(Loading){
+        return(
+            <div class="busy">
+                <ReactLoading type='bars' color='#006582' height={'20vw'} width={'20vw'} />
+            </div>
+        )
+    }else{
+        return (
+            <div>
+                <h1>TODO: dashboard space </h1>
+                <ol>
+                    <li>semantic progress overview</li>
+                    <li>git overview</li>
+                    <li>publish overview</li>
+                </ol>
+                <a href={`/spaces/${SpaceId}/all_files`}>see all files</a>
+                <div><Doughnut data={Graphdata} width="200px" height="200px" options={{maintainAspectRatio:false, plugins:{legend:{display:false}}}}/></div>
+                
+            </div>
+        )
+    }
+    
 }
 
 export default SpaceOverviewPage
