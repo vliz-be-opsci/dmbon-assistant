b7467ab7c47d75e908e9399c3ed6439ec00ce6a1
cedricdecruw <30471340+cedricdcc@users.noreply.github.com>
2022-05-17 15:56:51 +0200

    Update frontend_run.sh

--- frontend/frontend_run.sh
+++ frontend/frontend_run.sh
@@ -1,5 +1,7 @@
-#! /bin/sh
+#! /bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd ${SCRIPT_DIR}
 # first see if node is installed on the users system
 node -v
 if [ $? -ne 0 ]; then
@@ -7,11 +9,14 @@ if [ $? -ne 0 ]; then
     echo "Please install Node.js and try again."
     exit 1
 fi
-    echo "Node.js is installed on your system."
-    echo "Installing frontend dependencies..."
-    npm install
-    echo "React.js is installed on your system."
-    echo "Running frontend..."
-    python -m webbrowser http://localhost:3000
-    export NODE_OPTIONS=--openssl-legacy-provider
-    npm run start
+echo "Node.js is installed on your system."
+echo "Installing frontend dependencies..."
+npm install
+npm install -g serve
+echo "React.js is installed on your system."
+echo "Running frontend..."
+python -m webbrowser http://localhost:1207
+serve -s build -l 1207
+echo "Frontend is running."
+echo "Press Ctrl+C to stop."
+    
