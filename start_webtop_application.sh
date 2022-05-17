44b611a7b5f253a2fae53f79da640c214d6b8a57
Marc Portier <marc.portier@gmail.com>
2022-05-17 15:55:49 +0200

    slight fixes to the provided shell scripts

--- start_webtop_application.sh
+++ start_webtop_application.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /usr/bin/env bash
 
 # open multiple terminals and execute the following commands:
 # cd /Users/<username>/webtop/frontend
@@ -6,7 +6,5 @@
 # cd /Users/<username>/webtop/backend
 # sh start-api-server.sh
 
-cd backend/
-sh start-api-server.sh & # the & is to run the command in the background
-cd ../frontend/
-sh frontend_run.sh &
+(cd backend/ && sh start-api-server.sh & ) # the & is to run the command in the background
+(cd frontend/ && sh frontend_run.sh & ) 
