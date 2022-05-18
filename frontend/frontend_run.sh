2e2379fbaf31b4f308fba2c2705f104643966cd3
Marc Portier <marc.portier@gmail.com>
2022-05-18 16:45:35 +0200

    use the _build script + keep using port 3000 (so not to have to change cors settings)

--- frontend/frontend_run.sh
+++ frontend/frontend_run.sh
@@ -1,7 +1,8 @@
-#! /bin/bash
+#! /usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${SCRIPT_DIR}
+
 # first see if node is installed on the users system
 node -v
 if [ $? -ne 0 ]; then
@@ -9,14 +10,22 @@ if [ $? -ne 0 ]; then
     echo "Please install Node.js and try again."
     exit 1
 fi
+
 echo "Node.js is installed on your system."
 echo "Installing frontend dependencies..."
 npm install
+
+echo "building the local one pager web site"
+./frontend_build.sh
+
+echo "installing local webserver srevice"
 npm install -g serve
+
 echo "React.js is installed on your system."
 echo "Running frontend..."
-python -m webbrowser http://localhost:1207
-serve -s build -l 1207
+python -m webbrowser http://localhost:3000/
+serve -s build -l 3000
+
 echo "Frontend is running."
 echo "Press Ctrl+C to stop."
     
