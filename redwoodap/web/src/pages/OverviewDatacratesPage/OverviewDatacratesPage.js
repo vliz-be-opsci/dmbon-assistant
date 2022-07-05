644f1deb3dacced932c765b83271b5bac01ecf86
cedricdecruw <30471340+cedricdcc@users.noreply.github.com>
2022-07-05 18:11:04 +0200

    changed datacrate pages

--- redwoodap/web/src/pages/OverviewDatacratesPage/OverviewDatacratesPage.js
+++ redwoodap/web/src/pages/OverviewDatacratesPage/OverviewDatacratesPage.js
@@ -1,55 +1,55 @@
-import React, { useRef, useEffect } from 'react'
-
-import axios from 'axios'
+import React, { useRef, useState, useEffect } from 'react'
 
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 import Taskbox from 'src/components/Taskbox/Taskbox'
-import useStore from 'src/store'
-import { api_url, getAllSpaces } from 'src/utils/AxiosRequestsHandlers'
+import { getAllDatacrates } from 'src/utils/AxiosRequestsHandlers'
 
 const OverviewDatacratesPage = () => {
   //const for spaces from the store
-  const all_spaces = useStore((state) => state.spaces)
-  const SettingSpaces = useStore((state) => state.setSpaces)
+  const [loading, setLoading] = useState(true)
+  const [all_spaces, setSpaces] = useState([]);
 
-  const setSpacess = async () => {
-    axios.get(`${api_url}/spaces`).then(
-      (res) => {
-        console.log(res.data)
-        SettingSpaces(res.data)
-      },
-      (error) => {
-        console.log(error)
+  useEffect(() => {
+    getAllDatacrates()
+      .then(res => {
+        setSpaces(res.data)
+        setLoading(false)
       }
+      )
+      .catch(err => {
+        console.log(err)
+      }
+      )
+  }, [])
+
+
+  //render
+  if(loading) {
+    return <div>Loading...</div>
+  }
+  else{
+    return (
+      <>
+        <MetaTags
+          title="OverviewDatacrates"
+          description="OverviewDatacrates page"
+        />
+        <h1>OverviewDatacratesPage</h1>
+        {all_spaces.map((space) => (
+          <>
+            <Taskbox
+              type="running"
+              name={space.name}
+              description={space.storage_path}
+            />
+          </>
+        ))}
+      </>
     )
   }
 
-  useEffect(() => {
-    setSpacess()
-  }, [])
-
-  return (
-    <>
-      <MetaTags
-        title="OverviewDatacrates"
-        description="OverviewDatacrates page"
-      />
-
-      <h1>OverviewDatacratesPage</h1>
-
-      {all_spaces.map((space) => (
-        <>
-          <Taskbox
-            type="running"
-            name={space.name}
-            description={space.storage_path}
-          />
-        </>
-      ))}
-    </>
-  )
 }
 
 export default OverviewDatacratesPage
