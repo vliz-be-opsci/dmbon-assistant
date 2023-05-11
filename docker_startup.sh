604a4eb966021f5f56db7168b07386323790ff2e
cedricd <cedricdecruw@gmail.com>
2023-05-11 11:28:17 +0200

    reworked ssh key generate
    reworked how to ssh key would be generated
    made preperations by making a .dmbon folder in the ~/.dmbon
    
    TODO: discuss with marc why his ssh-keygen.sh fails

--- docker_startup.sh
+++ docker_startup.sh
@@ -3,6 +3,40 @@ cd shell_scripts
 sh ./make-git-sshkey.sh
 cd ..
 
+#check if the ssh key exists in the .ssh folder
+# if not execute the following command to generate the ssh key
+#  ssh-keygen -t ed25519 -C "your_email@example.com"
+#first ask the user for the email adress they use for github
+
+#if ssh key exists echo "ssh key exists" and continue
+if [ -f ~/.ssh/id_ed25519 ]; then
+    echo "ssh key exists continuing with docker-compose up"
+fi
+
+#check if C:\\dmbon exists on disk if not create it
+if [ ! -d ~/.dmbon ]; then
+    mkdir ~/.dmbon
+fi
+
+if [ ! -f ~/.ssh/id_ed25519 ]; then
+    #ask user for gh email adress
+    echo "Enter the email adress you use for github: "
+    echo "============================================="
+    read gh_email
+    echo "============================================="
+    echo " the ssh keygen will be generated with the email adress: $gh_email"
+    echo " press continue on all the following prompts"
+    echo "============================================="
+    #generate ssh key
+    ssh-keygen -t ed25519 -C "$gh_email"
+fi
+
+#check if the ssh key exists in the .ssh folder
+if [ ! -f ~/.ssh/id_ed25519 ]; then
+    echo "ssh key does not exist"
+    exit 1
+fi
+
 #make variable that will hold the contents of the ssh key
 ssh_prv_key="$(cat ~/.ssh/id_ed25519)"
 ssh_pub_key="$(cat ~/.ssh/id_ed25519.pub)"
